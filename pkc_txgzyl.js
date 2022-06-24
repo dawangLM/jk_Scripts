4dedd63bdb57144a8f113453250d2ccb15963076
Aurora <50320437+Aurora209@users.noreply.github.com>
2022-06-24 11:39:01 +0800

    Update

--- pkc_txgzyl.js
+++ pkc_txgzyl.js
@@ -23,11 +23,11 @@ https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/ma
 
 [task_local]
 #获取body后执行
-10 10 * * * https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/jk_script/pkc_txgzyl.js, tag=PKC-特效关注有礼, enabled=false
+1 1 1 1 * https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/jk_script/pkc_txgzyl.js, tag=PKC-特效关注有礼, enabled=false
 
 
 */
-const $ = new Env('21 PKC关注有礼-特效');
+const $ = new Env('PKC关注有礼-特效');
 let cookiesArr = [], cookie = '',  notify,  allMessage = '' ;
 const logs = 0; // 0为关闭日志，1为开启
 $.message = '';
@@ -373,7 +373,12 @@ async function getMyPing(timeout = 500) {
                          $.log(`${JSON.stringify(err)}`);
                      }else {
                          $.data = JSON.parse(data);
-                         // console.log(JSON.stringify($.data,null, '\t'));
+												 rep_cookies = resp.headers['set-cookie'];
+												// console.log(rep_cookies);
+												r_cookie='';
+												for(var c in rep_cookies){
+														r_cookie += rep_cookies[c].split(" ")[0];
+												}
                           if($.data.result){
                               username=$.data.data.nickname;
                               pin=encodeURIComponent($.data.data.secretPin);
