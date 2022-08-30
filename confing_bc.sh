a579dfc994a9c4969ad74f4359f16d7046d46761
Aurora <50320437+Aurora209@users.noreply.github.com>
2022-08-30 20:51:59 +0800

    Update

---
+++ confing_bc.sh
@@ -0,0 +1,146 @@
+#  Bot 监控文件自动变量 2022-08-30
+
+##  粉丝互动 jd_wxFansinter.js
+export wxFansInterActionActivity="*"
+
+##  joy抽奖机通用 jd_lottery.js
+export JD_Lottery="cd99100415734795a4798547287af02c"
+
+##  自动车-联合关注+加购+分享领豆 fav_and_addcart.js
+export welfare="*"
+
+##  自动车-电脑配件 jd_computer.js
+export computer_activityId="*"
+
+##  邀好友赢大礼 jd_inviteFriendsGift.py
+export jd_inv_authorCode="*"
+
+##  自动车-jd_joyjd_open通用ID任务 jd_joyjd_open.js
+export comm_activityIDList="*"
+
+##  入会开卡领取礼包 jd_OpenCard_Force.js
+export VENDER_ID="*"
+
+##  整点京豆雨 jd_redrain.js
+export jd_redrain_url="*"
+
+##  半点京豆雨 jd_redrain_half.js
+export jd_redrain_half_url="*"
+
+##  lz分享有礼 jd_share2.js
+export jd_fxyl_activityId="*"
+
+##  M收藏有礼 m_jd_fav_shop_gift.js
+export M_FAV_SHOP_ARGV="745211_749959"
+
+##  M关注有礼 m_jd_follow_shop.js
+export M_FOLLOW_SHOP_ARGV="682040_685798"
+
+##  M加购有礼 m_jd_wx_addCart.js
+export M_WX_ADD_CART_URL="https://lzkj-isv.isvjcloud.com/wxCollectionActivity/activity2/activity?activityId=e3d1ba1b89974a89af8ccfc18d2edee1"
+
+##  PKC关注有礼 pkc_gzyl.js
+export PKC_GZYL="*"
+
+##  赚京豆-瓜分京豆 pkc_zjd.js
+export zjdbody="*"
+
+##  M集卡抽奖 pkc_zjd.js
+export M_WX_COLLECT_CARD_URL="https://lzkjdz-isv.isvjcloud.com/wxCollectCard/activity/6511342?activityId=2d92dd76fb5541b88ad94193416dcbb4&shopid=1000101952"
+
+##  M幸运抽奖 m_jd_wx_luckDraw.js
+export M_WX_LUCK_DRAW_URL="https://cjhy-isv.isvjcloud.com/wxDrawActivity/activity/5198842?activityId=8aecbf6afbf24a93953ca421028201a0"
+
+##  自动车-盲盒任务抽京豆 jd_mhtask.js
+export jd_mhurlList="*"
+
+##  购物车锦鲤通用活动 jd_wxCartKoi.js
+export jd_wxCartKoi_activityId="1437a89d128740c88105cc9363fe6db1"
+
+##  集卡抽奖通用活动 jd_wxCollectCard.js
+export jd_wxCollectCard_activityId="2d92dd76fb5541b88ad94193416dcbb4"
+
+##  粉丝互动通用活动 jd_wxFansInterActionActivity.js
+export jd_wxFansInterActionActivity_activityId="fb9ffaa97eff49ecbcba6f0e38df1113"
+
+##  LZ让福袋飞通用活动 jd_wxUnPackingActivity.js
+export jd_wxUnPackingActivity_activityId="*"
+
+##  众筹许愿池 jd_wish.js
+export wish_appIdArrList="*"
+
+##  PKC-特效关注有礼 jd_txgzyl.js
+export PKC_TXGZYL="f2d7f3f18db54b4d8da304af6c9be32f"
+
+##  微定制瓜分福袋-加密 jd_wdzfd.js
+export jd_wdzfd_activityId=""
+
+##  大牌联合通用开卡 jd_opencardDPLHTY.js
+export DPLHTY="cf6495df4b604e95a7aad_220827"
+
+##  超级无线店铺签到 jd_sevenDay.js
+export SEVENDAY_LIST=""
+
+##  关注店铺抽奖 jd_wxShopFollowActivity.js
+export jd_wxShopFollowActivity_activityId="8fda737fc2ad4991b83d55fbf50c42d8"
+
+##  邀请赢大礼 jd_yqhy.py
+export yhypin="20462764-81081284" 
+export yhyactivityId="dVF7gQUVKyUcuSsVhuya5d2XD4F"
+export yhyauthorCode="1567c12daeff478e939700b1c31a0bf7"
+
+##  微定制瓜分京豆-加密 jd_wdz.js
+export jd_wdz_activityId="90d69af91c4d46f881e7bec083821c56"
+
+##  JOY通用开卡活动 jd_joyopen.js
+export JD_JOYOPEN=""
+
+##  LZ加购有礼 jd_lzaddCart.js
+export jd_lzaddCart_activityId=""
+
+##  CJ组队瓜分京豆 jd_cjzdgf.js
+export jd_cjhy_activityId="641bfb7756d741559c01eb1fedc64544"
+export jd_cjhy_activityUrl="https://cjhydz-isv.isvjcloud.com"
+
+##  LZ组队瓜分京豆 jd_zdjr.js
+export jd_zdjr_activityId="e5075a5d0765473db2c14f26f0ff1b64"
+export jd_zdjr_activityUrl="https://lzkjdz-isv.isvjcloud.com"
+
+##  读秒拼手速通用活动 jd_wxSecond.js
+export jd_wxSecond_activityId="4453e9fa4e2c4ae49e1a9dad13c4d49e"
+export jd_wxSecond_addCart=""
+
+##  LZ刮刮乐抽奖通用活动-加密 jd_drawCenter.js
+export jd_drawCenter_activityId="e7ddbf102a6e4759954c6471ae5b7f65"
+export jd_drawCenter_addCart=""
+
+##  幸运抽奖通用活动-加密 jd_luck_draw.js
+export LUCK_DRAW_URL=""
+export LUCK_DRAW_OPENCARD=""
+
+##  分享有礼 jd_wxShareActivity.js
+export jd_wxShareActivity_activityId=""
+export jd_wxShareActivity_helpnum=""
+
+##  LZ店铺游戏 jd_wxgame.js
+export jd_wxgame_activityId="e77d5f22531040ddb5127ceb5742bca8"
+export jd_wxgame_addCart=""
+
+## M老虎机抽奖 m_jd_wx_centerDraw.js
+export M_WX_CENTER_DRAW_URL="https://lzkj-isv.isvjcloud.com/drawCenter/activity/81a44779ba0548828207fa167ee190d9?activityId=81a44779ba0548828207fa167ee190d9&adsource=tg_xuanFuTuBiao"
+export SHARE_ACTIVITY_ID="9c97aded01e44f1387b0191718660a29"
+
+## 品类联合通用 jd_opencardPL.js
+export opencard_draw="5" ## 抽奖次数
+export opencard_pl=""
+
+## 加购有礼通用-加密 jd_wxCollectionActivity.js
+export jd_wxCollectionActivity_activityUrl=" "
+export jd_wxCollectionActivity_openCard=""
+export jd_wxCollectionActivity_num="6"
+
+
+
+
+
+
