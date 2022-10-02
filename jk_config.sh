6c32f5d9005d97c978c5d3d69370c096101bb998
Aurora <50320437+Aurora209@users.noreply.github.com>
2022-10-02 16:49:46 +0800

    Update

---
+++ jk_config.sh
@@ -0,0 +1,245 @@
+# Bot 监控文件自动变量 2022-10-02
+## 分享有礼 jd_wxShareActivity.js
+export jd_wxShareActivity_activityId="2022-10-02"
+
+## lzkj幸运抽奖 jd_lzkj_wxDrawActivity.js
+export jd_lzkj_wxDrawActivity_Id="2022-10-02"
+
+## lzkj加购物车抽奖 jd_lzkj_wxCollectionActivity.js
+export jd_lzkj_wxCollectionActivityId="2022-10-02"
+
+## cjhy幸运抽奖 jd_cjhy_wxDrawActivity.js
+export jd_cjhy_wxDrawActivity_Id="2022-10-02"
+
+## cjhy加购物车抽奖 jd_cjhy_wxCollectionActivity.js
+export jd_cjhy_wxCollectionActivityId="2022-10-02"
+
+## 邀好友赢大礼 jd_inviteFriendsGift.py
+export jd_inv_authorCode="2022-10-02"
+
+## 加购有礼-JK jd_wxCollectionActivity.py
+export jd_wxCollectionActivityUrl="2022-10-02"
+
+## cjhy游戏活动 jd_cjhy_wxGameActivity.js
+export jd_cjhy_wxGameActivity_activityId="2022-10-02"
+
+## CJ组队瓜分京豆 jd_cjzdgf.js
+export jd_cjhy_activityId="2022-10-02"
+export jd_cjhy_activityUrl="2022-10-02"
+
+## LZ组队瓜分京豆 jd_zdjr.js
+export jd_zdjr_activityId="2022-10-02"
+export jd_zdjr_activityUrl="2022-10-02"
+
+## 店铺礼包特效 jd_wxShopGift.js
+export jd_wxShopGift_activityUrl="2022-10-02"
+
+## 关注店铺抽奖 jd_wxShopFollowActivity.js
+export jd_wxShopFollowActivity_activityId="2022-10-02"
+
+## lzkj店铺礼包 jd_lzkj_wxShopGift.js
+export jd_lzkj_wxShopGift_ids="2022-10-02"
+
+## cjhy店铺礼包 jd_cjhy_wxShopGift.js
+export jd_cjhy_wxShopGift_ids="2022-10-02"
+
+## 分享有礼 jd_share.js
+export jd_fxyl_activityId="2022-10-02"
+
+## lzkj关注店铺有礼 jd_lzkj_wxShopFollowActivity.js
+export jd_lzkj_wxShopFollowActivity_activityId="2022-10-02"
+
+## lzkj知识超人 jd_lzkj_wxKnowledgeActivity.js
+export jd_lzkj_wxKnowledgeActivity_activityId="2022-10-02"
+
+## lzkj游戏活动 jd_lzkj_wxGameActivity.js
+export jd_lzkj_wxGameActivity_activityId="2022-10-02"
+
+## lzkj盖楼有礼 jd_lzkj_wxBuildActivity.js
+export jd_lzkj_wxBuildActivity_activityId="2022-10-02"
+
+## lzkj签到有礼 jd_lzkj_signActivity2.js
+export jd_lzkj_signActivity2_ids="2022-10-02"
+
+## lzkj七日签到 jd_lzkj_sevenDay.js
+export lzkj_sevenDay_ids="2022-10-02"
+
+## lzkj每日抢 jd_lzkj_daily.js
+export jd_lzkj_daily_ids="2022-10-02"
+
+## cjhy关注店铺有礼 jd_cjhy_wxShopFollowActivity.js
+export jd_cjhy_wxShopFollowActivity_activityId="2022-10-02"
+
+## cjhy生日礼 jd_cjhy_wxMcLevelAndBirthGifts.js
+export jd_cjhy_wxMcLevelAndBirthGifts_ids="2022-10-02"
+
+## cjhy知识超人 jd_cjhy_wxKnowledgeActivity.js
+export jd_cjhy_wxKnowledgeActivity_activityId="2022-10-02"
+
+## cjhy签到有礼 jd_cjhy_signActivity.js
+export jd_cjhy_signActivity_ids="2022-10-02"
+export jd_cjhy_signActivity_num="2022-10-02"
+export jd_cjhy_signActivity_openCard="2022-10-02"
+
+## cjhy七日签到 jd_cjhy_sevenDay.js
+export jd_cjhy_sevenDay_ids="2022-10-02"
+export jd_cjhy_sevenDay_num="2022-10-02"
+export jd_cjhy_sevenDay_openCard="2022-10-02"
+
+## cjhy每日抢 jd_cjhy_daily.js
+export jd_cjhy_daily_ids="2022-10-02"
+export jd_cjhy_daily_num="2022-10-02"
+export jd_cjhy_daily_openCard="2022-10-02"
+
+## cjhy完善信息 jd_cjhy_completeInfoActivity.js
+export jd_cjhy_completeInfoActivity_Ids="2022-10-02"
+
+## CJ每日抢好礼通用活动 jd_cjdaily.js
+export jd_cjdaily_activityId="2022-10-02"
+
+## LZ每日抢好礼通用活动 jd_daily.js
+export jd_daily_activityId="2022-10-02"
+
+## 大牌联合通用开卡 jd_opencardDPLHTY.js
+export DPLHTY="2022-10-02"
+
+## 粉丝互动通用活动 jd_wxFansInterActionActivity.js
+export jd_wxFansInterActionActivity_activityId="2022-10-02"
+
+## LZ店铺游戏 jd_wxgame.js
+export jd_wxgame_activityId="2022-10-02"
+export jd_wxgame_addCart="2022-10-02"
+
+## LZ让福袋飞通用活动 jd_wxUnPackingActivity.js
+export jd_wxUnPackingActivity_activityId="2022-10-02"
+
+## 店铺特效关注有礼-JK jd_wxShopGift.py
+export jd_wxShopGiftId="2022-10-02"
+
+## 完善信息有礼-监控脚本 jd_wxCompleteInfo.py
+export jd_wxCompleteInfoId="2022-10-02"
+
+## 生日礼包-监控脚本 jd_wxBirthGifts.py
+export jd_wxBirthGiftsId="2022-10-02"
+
+## 微定制组队瓜分-监控脚本 jd_wdz.py
+export jd_wdz_activityId="2022-10-02"
+
+## 店铺会员礼包-监控脚本 jd_shopCollectGift.py
+export jd_shopCollectGiftId=" "
+
+## 通用开卡-joinCommon系列 jd_joinCommon_opencard.py
+export jd_joinCommonId=" "
+
+## jinggeng邀请入会有礼 jd_jinggengInvite.py
+export redis_url=" "
+
+## 微定制-开福袋 jd_wdz_openLuckBag.js
+export jd_wdz_openLuckBag_activityId="2022-10-02"
+
+## 店铺抽奖通用活动-加密 jd_luck_draw.js
+export LUCK_DRAW_URL="2022-10-02"
+export LUCK_DRAW_OPENCARD="2022-10-02"
+export LUCK_DRAW_NOTIFY="2022-10-02"
+export LUCK_DRAW_NUM="5"
+
+## LZ盖楼有礼 jd_wxBuildActivity.js
+export jd_wxBuildActivity_activityId="2022-10-02"
+
+## 入会开卡领取礼包通用 jd_card_force.js
+export VENDER_ID="2022-10-02"
+export OPENCARD_BEAN="2022-10-02"
+
+## joy抽奖机通用 jd_lottery.js
+export jd_lotteryId="2022-10-02"
+
+## joy通用开卡 jd_opencardJoy.js
+export jd_joyOpenId="2022-10-02"
+
+## 关注有礼-自动解析通用 jd_whx_drawShopGift.js
+export whx_drawShopGift="2022-10-02"
+
+## 品类联合-通用脚本 jd_lzdz_categoryUnion.js
+export categoryUnion_activityId="2022-10-02"
+
+## 品类联合抽奖 jd_categoryUnion_draw.js
+export jd_categoryUnion_activityId="2022-10-02"
+
+## 品类联合任务 jd_categoryUnion.js
+export jd_categoryUnion_activityId="2022-10-02"
+
+## 加购有礼通用-加密 jd_wxCollectionActivity.js
+export jd_wxCollectionActivity_activityUrl="2022-10-02"
+export jd_wxCollectionActivity_openCard="2022-10-02"
+export jd_wxCollectionActivity_num="2022-10-02"
+
+## M微定制 m_jd_wx_microDz.js
+export M_WX_WDZ_ID="2022-10-02"
+
+## 生日礼包和会员等级礼包 jd_wxMcLevelAndBirthGifts.js
+export jd_wxMcLevelAndBirthGifts_activityId="2022-10-02"
+
+## CJ完善有礼-加密 jd_completeInfoActivity.js
+export jd_completeInfoActivity_activityId="2022-10-02"
+export jd_completeInfoActivity_venderId="2022-10-02"
+
+## 通用开卡-shopLeague系列 jd_shopLeague_opencard.py
+export jd_shopLeagueId="2022-10-02"
+
+## M老虎机抽奖 m_jd_wx_centerDraw.js
+export M_WX_CENTER_DRAW_URL="2022-10-02"
+
+## M幸运抽奖 m_jd_wx_luckDraw.js
+export M_WX_LUCK_DRAW_URL="2022-10-02"
+
+## M加购有礼 m_jd_wx_addCart.js
+export M_WX_ADD_CART_URL="2022-10-02"
+
+## 品类联合 通用 jd_opencardPL.js
+export opencard_pl="2022-10-02"
+export opencard_draw=" "
+## M关注有礼无线 m_jd_wx_shopGift.js
+export M_WX_SHOP_GIFT_URL="2022-10-02"
+
+## M关注抽奖 m_jd_wx_followDraw.js
+export M_WX_FOLLOW_DRAW_URL="2022-10-02"
+
+## M盖楼领奖 m_jd_wx_buildDraw.js
+export M_WX_BUILD_DRAW_URL="2022-10-02"
+
+## M关注有礼 m_jd_follow_shop.js
+export M_FOLLOW_SHOP_ARGV="2022-10-02"
+
+## LZ刮刮乐抽奖通用活动-加密 jd_drawCenter.js
+export jd_drawCenter_activityId="2022-10-02"
+export jd_drawCenter_addCart="2022-10-02"
+
+## 读秒拼手速通用活动 jd_wxSecond.js
+export jd_wxSecond_activityId="2022-10-02"
+export jd_wxSecond_addCart="2022-10-02"
+
+## 邀请赢大礼 jd_yqhy.py
+export yhyactivityId="2022-10-02"
+export yhyauthorCode="2022-10-02"
+
+## PKC-特效关注有礼 jd_txgzyl.js
+export PKC_TXGZYL="2022-10-02"
+
+## 集卡抽奖通用活动 jd_wxCollectCard.js
+export jd_wxCollectCard_activityId="2022-10-02"
+
+## 购物车锦鲤通用活动 jd_wxCartKoi.js
+export jd_wxCartKoi_activityId="2022-10-02"
+
+## M集卡抽奖 m_jd_wx_collectCard.js
+export M_WX_COLLECT_CARD_URL="2022-10-02"
+
+## M收藏有礼 m_jd_fav_shop_gift.js
+export M_FAV_SHOP_ARGV="2022-10-02"
+
+## 自动车-女装盲盒抽京豆 jd_nzmh.js
+export jd_nzmhurl="2022-10-02"
+
+## 自动车-jd_joyjd_open通用ID任务 jd_joyjd_open.js
+export comm_activityIDList="2022-10-02"
+
